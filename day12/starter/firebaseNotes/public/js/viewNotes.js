eb70609b265d2ddf11fc5788340dab3fbff1d39d
B1QUAD <accntmail.745@gmail.com>
2021-07-27 21:50:09 +0000

    Day 12 - Completed medium

---
+++ day12/starter/firebaseNotes/public/js/viewNotes.js
@@ -0,0 +1,98 @@
+/**
+ * Pseudocode:
+ * When the page loads check user login state
+ *  If not logged in, redirect to log in page
+ *  If logged in, get user's notes from DB and display them
+ */
+
+window.onload = () => {
+    const ideaTitle = document.querySelector('#ideaTitle');
+    
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // Logged in
+            const googleUid = user.uid;
+            getNotes(googleUid);
+            ideaTitle.innerHTML = `Some of ${user.displayName}'s Greatest Ideas Started Here`
+        } else {
+            // Not logged in
+            window.location = 'index.html'
+        }
+    });
+}
+
+const getNotes = (userId) => {
+    console.log(userId);
+    
+    const userRef = firebase.database().ref(`users/${userId}`);
+    userRef.on('value', snapshot => {
+        writeNotesToHtml(snapshot.val());
+    });
+}
+
+const writeNotesToHtml = (data) => {
+    const noteRenderArea = document.querySelector('#app');
+    
+    for (let noteKey in data) {
+        noteRenderArea.appendChild(createHtmlForNote(data[noteKey]))
+    }
+}
+
+const color = [
+    'has-background-success-dark',
+    'has-background-warning-dark',
+    'has-background-danger-dark'
+];
+
+function getColor() {
+    return color[Math.floor(Math.random() * color.length)];
+}
+
+const createHtmlForNote = (note) => {
+    // Create the element and put in the note data
+    // const noteRenderArea = document.querySelector('#app');
+    let columnDiv = document.createElement('div');
+    columnDiv.classList.add('column');
+    columnDiv.classList.add('is-one-quarter');
+
+    let cardDiv = document.createElement('div');
+    cardDiv.classList.add(`card`);
+    cardDiv.classList.add(`${getColor()}`);
+    columnDiv.appendChild(cardDiv);
+
+    let header = document.createElement('header');
+    header.classList.add('card-header');
+
+    let p = document.createElement('p');
+    p.classList.add('card-header-title');
+    p.innerHTML = `Title:<br>${note.title}`;
+    header.appendChild(p);
+    cardDiv.appendChild(header);
+
+    let cardContent = document.createElement('div');
+    cardContent.classList.add('card-content');
+    cardDiv.appendChild(cardContent);
+
+    let content = document.createElement('div');
+    content.classList.add('content');
+    content.innerHTML = `Note:<br>${note.text.length > 0 ? note.text : 'Empty note. (ಥ﹏ಥ)'}`;
+    cardContent.appendChild(content);
+
+    return columnDiv;
+    // return `<div class="column is-one-fourth">
+    //             <div class="card ${getColor()}">
+    //                 <header class = "card-header">
+    //                         <p class="card-header-title">
+    //                             Title:<br>${note.title}
+    //                         </p>
+    //                 </header>
+    //                 <div class="card-content">
+    //                     <div class="content">
+    //                         Note:<br>${note.text.length > 0 ? note.text : 'Empty note. (ಥ﹏ಥ)'}
+    //                     </div>
+    //                 </div>
+    //             </div>
+    //         </div>`;
+
+    // return `${note.title}, ${note.text} | `
+}
